de329451670355eafb90a98f7c969ba9e671e728
salsaazka <salsabilaazkaputri@smkwikrama.sch.id>
2022-12-22 10:01:27 +0700

    upload

---
+++ app/Http/Controllers/PpdbController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ppdb;
+use Illuminate\Http\Request;
+
+class PpdbController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ppdb  $ppdb
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ppdb $ppdb)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ppdb  $ppdb
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ppdb $ppdb)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ppdb  $ppdb
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ppdb $ppdb)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ppdb  $ppdb
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ppdb $ppdb)
+    {
+        //
+    }
+}
