3bfe2ddd6c7a030a6eac1575b0d9a9c246ca9bd6
Yogapratama1109 <yoga110011@gmail.com>
2022-12-24 16:22:18 +0700

    update

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.base-home')
 
+@section('css')
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+@endsection
+
 @section('content')
     <div>
 
@@ -7,8 +12,9 @@
         <nav class="navbar navbar-expand-lg bg-transparant">
             <div class="container bg-light py-2 px-5 rounded-pill fixed-top">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -47,22 +53,135 @@
         {{-- End Navbar --}}
 
         {{-- Main Content --}}
+
+        {{-- Banner --}}
+
         <div class="banner card bg-dark text-white w-100 rounded-0" style="margin-top: -1rem">
-            <img src="https://mdbcdn.b-cdn.net/img/new/slides/017.webp" class="banner-img" alt="Stony Beach"/>
-            <div class="card-img-overlay d-flex flex-column justify-content-center" style="background-color: rgba(0, 0, 0, 0.3)">
+            <img src="https://mdbcdn.b-cdn.net/img/new/slides/017.webp" class="banner-img" alt="Stony Beach" />
+            <div class="card-img-overlay d-flex flex-column justify-content-center"
+                style="background-color: rgba(0, 0, 0, 0.3)">
                 <div class="container">
                     <h1 class="banner-title">PPDB TP 2023-2024</h1>
                     <h1 class="banner-title">SMK Wikrama Bogor</h1>
                     <p class="card-text">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellendus libero iusto fuga in voluptatem obcaecati tempora, eius ut cupiditate, ex cum quidem placeat corrupti illum recusandae beatae atque. Provident!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repellendus libero iusto fuga in
+                        voluptatem obcaecati tempora, eius ut cupiditate, ex cum quidem placeat corrupti illum recusandae
+                        beatae atque. Provident!
                     </p>
                     <a href="" class="btn btn-warning rounded-0 p-3 mt-3 text-white">
                         Pendaftaran PPDB
                     </a>
                 </div>
             </div>
-          </div>
+        </div>
+
+        {{-- End Banner --}}
+
+        {{-- Info --}}
+
+        <div class="container">
+            <div class="info card shadow">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <center>1</center>
+                        </div>
+                        <div class="col">
+                            <center>1</center>
+                        </div>
+                        <div class="col">
+                            <center>1</center>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        {{-- End Info --}}
+
+        {{-- Jurusan --}}
+        <div class="container">
+            <div class="jurusan">
+                <h2>Jurusan</h2>
+                <div class="your-class">
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                    <div class="card bg-black p-5 me-2">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque sit tenetur minima accusantium. In,
+                        impedit ipsa. Modi architecto magnam provident, blanditiis quidem earum animi obcaecati quod iste
+                        magni necessitatibus voluptas?
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- End Jurusan --}}
+
         {{-- End Main Content --}}
 
     </div>
+@endsection
+
+@section('js')
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script>
+        $('.your-class').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+    </script>
 @endsection
