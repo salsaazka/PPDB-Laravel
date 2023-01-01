eda495303b6e0122ec3349fa69d75289f3626ca3
Yogapratama1109 <yoga110011@gmail.com>
2023-01-01 10:19:24 +0700

    update

--- resources/views/layouts/base-user.blade.php
+++ resources/views/layouts/base-user.blade.php
@@ -124,7 +124,7 @@
                 </div>
 
                 <div class="d-flex justify-content-between align-items-center nav-input-container">
-                    <p class="mt-3">{{Auth::user()->name}}</p>
+                    <p class="mt-3">{{Auth::user()->email}}</p>
                     <button class="btn-notif d-none d-md-block"><img src="{{ url('assets/img/global/person.svg') }}"
                             alt=""></button>
                 </div>
